ec4d945b7be8f1f02855b90bbadecd056a27ddcf
aguestuser <aguestuser@riseup.net>
2016-02-16 15:24:04 -0500

    AG | Initial commit
    * Add MapViewManager
      * instantiates an OSM MapView with default config values
      * sets some zoom paramaeters
      * view constructed programatically (no xml)

---
+++ app/src/main/java/whereat/io/osmmapplayground/MainActivity.java
@@ -0,0 +1,27 @@
+package whereat.io.osmmapplayground;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapView mv = MapViewManager.createMapView(this);
+        setContentView(mv);
+
+    }
+
+}
---
+++ app/src/main/java/whereat/io/osmmapplayground/MapViewManager.java
@@ -0,0 +1,34 @@
+package whereat.io.osmmapplayground;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.util.ResourceProxyImpl;
+import org.osmdroid.views.MapView;
+
+/**
+ * Created by aguestuser on 2/16/16.
+ */
+
+public class MapViewManager {
+
+    public static MapView createMapView(Context context) {
+        MapView mv = new MapView(context);
+
+        mv.setTileSource(TileSourceFactory.MAPNIK);
+        mv.setBuiltInZoomControls(true);
+        mv.setMultiTouchControls(true);
+
+        IMapController mapController = mv.getController();
+        mapController.setZoom(13);
+        mapController.setCenter(new GeoPoint(40.7447038,-73.9870748));
+
+        return mv;
+    }
+}
