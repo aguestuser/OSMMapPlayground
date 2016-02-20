85d39140cbb4d4e1da4bbac9fddcc7cc920e0ca6
aguestuser <aguestuser@riseup.net>
2016-02-20 12:45:05 -0500

    AG | Tweak OSMMapViewManagerTest
    * add comment to line that manually sets view dimensions to make clear
      why it's necessary
    * reorder, consolidate assertions

--- app/src/test/java/whereat/io/osmmapplayground/MapViewManagerTest.java
+++ app/src/test/java/whereat/io/osmmapplayground/MapViewManagerTest.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
 import org.robolectric.Robolectric;
@@ -17,6 +16,7 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21, shadows=MyZoomButtonsController.class)
+
 public class MapViewManagerTest {
 
     @Test
@@ -24,18 +24,16 @@ public class MapViewManagerTest {
 
         Context context = Robolectric.buildActivity(MainActivity.class).create().resume().visible().get();
         MapView map = MapViewManager.createMapView(context);
-        map.layout(0, 0, 1920, 1080);
-        IGeoPoint ctr = map.getMapCenter();
+        map.layout(0, 0, 1920, 1080); // because robolectric views have dimension 0 x 0 by default
 
         assertThat(map).isInstanceOf(MapView.class);
 
         assertThat(map.getTileProvider().getTileSource()).isEqualTo(TileSourceFactory.MAPNIK);
         assertThat(map.canZoomIn()).isTrue();
         assertThat(map.canZoomOut()).isTrue();
-
         assertThat(map.getZoomLevel()).isEqualTo(13);
-        assertThat(isWithin(ctr.getLatitude(), 40.7447038, .0001)).isTrue();
-        assertThat(isWithin(ctr.getLongitude(), -73.9870748, .0001)).isTrue();
+        assertThat(isWithin(map.getMapCenter().getLatitude(), 40.7447038, .0001)).isTrue();
+        assertThat(isWithin(map.getMapCenter().getLongitude(), -73.9870748, .0001)).isTrue();
     }
 
     public boolean isWithin(double expected, double actual, double diff){
